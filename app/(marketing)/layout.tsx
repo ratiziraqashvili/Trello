78feab6edf3a61aa6cebc6f751994eaa66881e92
Rati <rziraqashvili@gmail.com>
2023-11-22 18:02:19 +0400

    Finish footer in home page

--- app/(marketing)/layout.tsx
+++ app/(marketing)/layout.tsx
@@ -1,3 +1,4 @@
+import { Footer } from "./_components/footer";
 import { Navbar } from "./_components/navbar";
 
 const MarketingLayout = ({ children }: { children: React.ReactNode }) => {
@@ -7,6 +8,7 @@ const MarketingLayout = ({ children }: { children: React.ReactNode }) => {
             <main className="pt-40 pb-20 bg-slate-100">
                 {children}
             </main>
+            <Footer />
         </div>
      );
 }
