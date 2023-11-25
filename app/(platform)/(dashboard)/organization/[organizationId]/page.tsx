98f59f6e1896a21f75d51ced8c18a2a4ed762b62
Rati <rziraqashvili@gmail.com>
2023-11-25 20:59:40 +0400

    new server action in next.js

--- app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -1,7 +1,29 @@
+import { db } from "@/lib/db";
+
 const OrganizationIdPage = () => {
+    async function create(formData: FormData) {
+        "use server"
+
+        const title = formData.get("title") as string;
+
+        await db.board.create({
+            data: {
+                title,
+            }
+        })
+    }
+
     return ( 
         <div className="">
-            zz
+            <form action={create}>
+                <input
+                id="title"
+                name="title"
+                required
+                placeholder="Enter a board title"
+                className="border-black border p-1"
+                />
+            </form>
         </div>
      );
 }
