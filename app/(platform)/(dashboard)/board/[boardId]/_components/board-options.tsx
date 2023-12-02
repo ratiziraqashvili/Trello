029b458458f949a72bc56929e1e8eb314c6eb9f8
Rati <rziraqashvili@gmail.com>
2023-12-02 15:18:48 +0400

    board delete

--- app/(platform)/(dashboard)/board/[boardId]/_components/board-options.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/board-options.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { deleteBoard } from "@/actions/delete-board";
 import { Button } from "@/components/ui/button";
 import {
   Popover,
@@ -7,13 +8,25 @@ import {
   PopoverContent,
   PopoverTrigger,
 } from "@/components/ui/popover";
+import { useAction } from "@/hooks/use-action";
 import { MoreHorizontal, X } from "lucide-react";
+import { toast } from "sonner";
 
 interface BoardOptionsProps {
   id: string;
 }
 
 export const BoardOptions = ({ id }: BoardOptionsProps) => {
+    const { execute, isLoading } = useAction(deleteBoard, {
+        onError: (error) => {
+            toast.error(error)
+        }
+    })
+
+    const onDelete = () => {
+        execute({ id })
+    }
+
   return (
     <Popover>
       <PopoverTrigger asChild>
@@ -35,7 +48,8 @@ export const BoardOptions = ({ id }: BoardOptionsProps) => {
         </PopoverClose>
         <Button
           variant="ghost"
-          onClick={() => {}}
+          onClick={onDelete}
+          disabled={isLoading}
           className="rounded-none w-full h-auto p-2 px-5 justify-start font-normal text-sm"
         >
           Delete this board
