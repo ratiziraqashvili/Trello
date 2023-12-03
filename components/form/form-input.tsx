55a187aa267c7492599871f49d97774ab84c0cf2
Rati <rziraqashvili@gmail.com>
2023-12-03 14:53:43 +0400

    adding list

--- components/form/form-input.tsx
+++ components/form/form-input.tsx
@@ -58,6 +58,7 @@ export const FormInput = forwardRef<HTMLInputElement, FormInputProps>(
           id={id}
           type={type}
           disabled={pending || disabled}
+          placeholder={placeholder}
           className={cn(
             "text-sm px-2 py-1 h-7",
             className,
