2bd33f920049d1db62306b98c6b90f4273dd2c64
Rati <rziraqashvili@gmail.com>
2023-12-08 13:02:36 +0400

    actions to copy card

--- components/modals/card-modal/index.tsx
+++ components/modals/card-modal/index.tsx
@@ -7,6 +7,7 @@ import { CardWithList } from "@/types";
 import { fetcher } from "@/lib/fetcher";
 import { Header } from "./header";
 import { Description } from "./description";
+import { Actions } from "./actions";
 
 export const CardModal = () => {
   const id = useCardModal((state) => state.id);
@@ -25,9 +26,14 @@ export const CardModal = () => {
         <div className="grid grid-cols-1 md:grid-cols-4 md:gap-4">
           <div className="col-span-3">
             <div className="w-full space-y-6">
-                {!cardData ? <Description.Skeleton /> : <Description data={cardData} />}
+              {!cardData ? (
+                <Description.Skeleton />
+              ) : (
+                <Description data={cardData} />
+              )}
             </div>
           </div>
+          {!cardData ? <Actions.Skeleton /> : <Actions data={cardData} />}
         </div>
       </DialogContent>
     </Dialog>
